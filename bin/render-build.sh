7b5071544d9e589d50e8a725c60eb6a5837ae425
MasudaTaiga <masuda.taiga@laiblitz.co.jp>
2026-04-20 15:31:33 +0900

    render-build.sh変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
+# 開発・テスト用Gemを除外してインストール
+bundle install --without development test
+
+# --trace を付けて詳細なエラーを表示させる
+bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+
+# マイグレーションにも --trace を追加
+bundle exec rake db:migrate --trace
